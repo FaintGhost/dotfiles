7de794a8172e6ce802cb72a52f551f079efc7e3c
FaintGhost <zhang.yaowei@live.com>
2026-01-08 17:45:39 +0800

    新增 nvim 插件配置文件，定义插件列表

---
+++ dot_config/nvim/lua/plugins/init.lua
@@ -0,0 +1,8 @@
+return {
+  "folke/neodev.nvim",
+  "folke/which-key.nvim",
+  { "folke/neoconf.nvim", cmd = "Neoconf" },
+  "nvim-lua/lsp-status.nvim",
+  "nvim-tree/nvim-tree.lua",
+  "nvim-tree/nvim-web-devicons"
+}
